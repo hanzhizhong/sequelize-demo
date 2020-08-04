8c1feac66d372614fa1084d517eaa3b46a657f96
hanzhizhong <kf_hz@sina.com>
2020-08-04 16:25:04 +0800

    fixed some bug

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-const {user}=require('./models')
+const {user,blog}=require('./models')
 
-user.findAll({raw:true,attributes:{exclude:'passwd'}}).then(ret=>{
+/* user.findAll({raw:true,attributes:{exclude:'passwd'}}).then(ret=>{
     console.log('sss',ret)
+}).catch(err=>{
+    console.log('error',err)
+}) */
+//console.log(user.tableName)
+//user.hasMany(blog,{foreignKey:'user_id'})
+blog.belongsTo(user,{foreignKey:'user_id'})
+blog.findAll({include:[{model:user,attributes:{exclude:['passwd']}}]}).then(ret=>{
+    console.log('blogs',JSON.stringify(ret,null,4))
+}).catch(err=>{
+    console.log('error',err)
 })
+
